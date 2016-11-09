9950c026fb52ed29bb3ca372a02c3ad267317d88
Etilawin <kimik812@gmail.com>
2016-11-09 16:44:29 +0100

    Organising the code

--- modules/functions.py
+++ modules/functions.py
@@ -1,3 +1,6 @@
+from PIL import Image
+
+
 def RGBToInt(rgb):
     """ Convert a list/tuple with 3 values in rgb one value """
     r, g, b = rgb
@@ -8,3 +11,15 @@ def intToRGB(number):
     """ Converts a number into an int tuple """
     r, g, b = number & 255, (number >> 8) & 255, (number >> 16)
     return (r, g, b)
+
+
+def encodeMessageInFile(image, message):
+    """ Creates a new image with the message encoded in it """
+    im = Image.open(image)
+    pix = im.load()
+    for x in range(im.size[0]):
+        for y in range(im.size[1]):
+            binary = bin(RGBToInt(pix[x, y]))[2:].zfill(24)
+            print(binary)
+    print(message)
+    im.show()
--- steganoFriend.py
+++ steganoFriend.py
@@ -1,29 +1,36 @@
 #!/usr/bin/env python3
 
-from PIL import Image
 import sys
 import getopt
 import modules
 
 
 def main(argv):
+    message = ''  # Plus facile à vérifier si jamais l'option est inutilisé
+    infile = ''
+    helpMessage = "test.py -i <inputfile> -m <message>"  # Message d'erreur
     try:
-        opts, args = getopt.getopt(argv, "hi::", ["ifile=", "ofile="])
+        # On récupère les arguments et leur valeurs
+        opts, args = getopt.getopt(argv, "hi:m:", ["ifile=", "message="])
     except getopt.GetoptError:
-        print('test.py -i <inputfile> <message>')
+        # Mauvaise utilisation des arguments
+        print(helpMessage)
         sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print('test.py -i <inputfile> <message>')
+    for opt, arg in opts:  # On parcours les arguments
+        if opt == '-h':  # Message d'aide demandé
+            print(helpMessage)
             sys.exit()
-        elif opt in ("-i", "--ifile"):
-            im = Image.open(arg)
-            pix = im.load()
-            for x in range(im.size[0]):
-                for y in range(im.size[1]):
-                    binary = bin(modules.RGBToInt(pix[x, y]))[2:].zfill(24)
-                    print(binary)
-            im.show()
+        elif opt in ("-i", "--ifile"):  # On récupère le nom du fichier
+            infile = arg
+        elif opt in ("-m", "--message"):  # On récupère le message à encoder
+            message = arg
+    try:  # Si jamais l'utilisateur ne met que le message ou que le fichier...
+        if message == '' or infile == '':
+            raise getopt.GetoptError  # On lève une erreur
+        modules.encodeMessageInFile(infile, message)
+    except:
+        print(helpMessage)
+        sys.exit(2)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
